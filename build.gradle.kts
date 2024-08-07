d046b197fa89490f6498981a10093809fc9995b4
Per Jørgen Walstrøm <pjwalstrom+github@gmail.com>
2024-08-07 13:59:59 +0200

    Prepare for Gradle 9.0 by declaring the test framework implementation on the runtime classpath

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,6 +17,7 @@ dependencies {
 
     testImplementation(platform("org.junit:junit-bom:5.10.3"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 tasks {
